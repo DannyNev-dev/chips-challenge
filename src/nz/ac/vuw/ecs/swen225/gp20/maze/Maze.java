173f6030c6f944a802a5c08befa59f38c5934562
Emanuel Evans <evanseman@ecs.vuw.ac.nz>
2020-09-16 23:09:25 +1200

    Add Tile interface and some of its implementations
    The structure for a tile (generic cell of a board) has been created. The initial focus has been on linking each tile sub-class to a unique name required by the Render for accessing the icon for displaying the tile.

--- src/nz/ac/vuw/ecs/swen225/gp20/maze/Maze.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/maze/Maze.java
@@ -1,18 +1,22 @@
 package nz.ac.vuw.ecs.swen225.gp20.maze;
 
+import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.Key;
+
 /**
- * This class manages the logic for the maze
+ * This class manages the logic for the maze.
+ * 
  * @author Emanuel Evans
  *
  */
 public class Maze {
-	
-	/**
-	 * Test setup
-	 * @param args initial arguments
-	 */
-	public static void main(String... args) {
-		System.out.println("Hello World");
-	}
+
+  /**
+   * Test setup.
+   * 
+   * @param args initial arguments
+   */
+  public static void main(String... args) {
+    System.out.println(Key.Colour.BLUE);
+  }
 
 }
---
+++ src/nz/ac/vuw/ecs/swen225/gp20/maze/tiles/Door.java
@@ -0,0 +1,38 @@
+package nz.ac.vuw.ecs.swen225.gp20.maze.tiles;
+
+import java.util.Collection;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Collectable;
+
+/**
+ * Create door objects which entity can pass across if they have collected the right key.
+ * 
+ * @author Emanuel Evans (ID: 300472656)
+ *
+ */
+public class Door implements Tile {
+  private final Key.Colour keyType;
+
+  // Ending of all the key names, stored to improve efficiency
+  private static final String suffix = "LockedTile";
+
+  /**
+   * Create a new looked tile with the colour of the key required to unlock them.
+   * 
+   * @param keyType the type of key required to open this door
+   */
+  public Door(Key.Colour keyType) {
+    this.keyType = keyType;
+  }
+
+  @Override
+  public String getName() {
+    return keyType.name().toLowerCase() + suffix;
+  }
+
+  @Override
+  public boolean isAccessible(Collection<Collectable> invetory) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+}
