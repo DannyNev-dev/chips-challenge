519389acf2f63759543c9f63a9994463613958cc
Jordan <jordan_890@hotmail.com>
2020-10-16 20:26:31 +1300

    Right step animation now working
    The images for the other directions of animation need to be made.

--- src/nz/ac/vuw/ecs/swen225/gp20/render/Render.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/render/Render.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
@@ -20,6 +21,7 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 import javafx.animation.Animation;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Board;
 import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
 import nz.ac.vuw.ecs.swen225.gp20.maze.Maze.SpecialEvent;
 import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.Tile;
@@ -43,7 +45,6 @@ public class Render {
 	public Render(Maze maze) {
 		this.maze = maze;
 		view = new View(maze);
-		System.out.println("yes");
 	}
 
 	/**
@@ -52,7 +53,7 @@ public class Render {
 	public void updateRender() {
 		// maze.getBoard();
 		view.refresh(maze);
-		view.updateLastPosition();
+		// view.updateLastPosition();
 	}
 
 	/**
@@ -79,10 +80,12 @@ class View {
 	private int xSize;
 	private int ySize;
 	Tile[][] board;
+	Board oldBoard;
 	private int tileSize = 50;
 	private final static int GAP = 2;
 	private BoardPanel BoardPanel;
 	private Point lastPosition = null;
+	private ArrayList<String> oldToken = new ArrayList<String>();
 
 	/**
 	 * Constructor for View
@@ -92,8 +95,9 @@ class View {
 	View(Maze maze) {
 		this.maze = maze;
 		board = maze.getBoard();
-		xSize = 9; //board.length;
-		ySize = 9; //board[0].length;
+		oldBoard = new Board(maze.getBoard());
+		xSize = 9; // board.length;
+		ySize = 9; // board[0].length;
 		dispList = new DispTile[xSize][ySize];
 		createAndShowGUI();
 	}
@@ -105,96 +109,198 @@ class View {
 	 * @param maze - the game state from the maze application
 	 */
 	void refresh(Maze maze) {
-		board = maze.getBoard();
-		//BoardPanel.removeAll();
+
 		Point p = maze.getPlayerPosition();
 
-		
-		System.out.println("Position: " + p);
-		//dispList = new DispTile[xSize][ySize];
+		String moveDirection = findDirection(p);
+		board = maze.getBoard();
+		// BoardPanel.removeAll();
+
+		Point lastP = new Point(lastPosition);
 
 		// ANIMATION
-		int delay = 1; // milliseconds
+		int delay = 100; // milliseconds
 		ActionListener taskPerformer = new ActionListener() {
 			int count = 0;
+			int dispPlayerX = -1;
+			int dispPlayerY = -1;
+
+			int dispLastPlayerX = -1;
+			int dispLastPlayerY = -1;
 
 			public void actionPerformed(ActionEvent evt) {
 				if (count == 4) {
 					return;
 				}
-				System.out.println(count);
+
 				// dispList = new DispTile[xSize][ySize];
-				int x = p.x-4;
-				for (int i = 0; i < xSize; i++) {
-					int y = p.y-4;
-					for (int j = 0; j < ySize; j++) {
-						if ((p.x == x && p.y == y) && lastPosition != p) {
-							System.out.println("test");
-							try {
-								final Image[] frames = {
-										ImageIO.read(new File("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/chipRight"
-												+ 1 + ".png")),
-										ImageIO.read(new File("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/chipRight"
-												+ 2 + ".png")),
-										ImageIO.read(new File("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/chipRight"
-												+ 3 + ".png")),
-										ImageIO.read(new File("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/chipRight"
-												+ 4 + ".png")) };
+				if (count == 0) {
+					System.out.println(count);
+					int x = p.x - 4;
+					for (int i = 0; i < xSize; i++) {
+						int y = p.y - 4;
+						for (int j = 0; j < ySize; j++) {
+							if (lastP.x == x && lastP.y == y) {
 
-								dispList[i][j].setIcon(new ImageIcon(frames[count]));
-								//BoardPanel.add(dispList[i][j]);
-								
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-
-						} else {
-							try {
-								Image[] frames = {
-										ImageIO.read(new File("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/"
-												+ board[x][y].getName() + ".png")),
-										ImageIO.read(new File("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/"
-												+ board[x][y].getName() + ".png")),
-										ImageIO.read(new File("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/"
-												+ board[x][y].getName() + ".png")),
-										ImageIO.read(new File("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/"
-												+ board[x][y].getName() + ".png")) };
-								if (dispList[i][j] != null) {
-									System.out.println("spam");
-
-									
-									dispList[i][j].setIcon(new ImageIcon(frames[count]));
-									//BoardPanel.add(dispList[i][j]);
-									
+								dispLastPlayerX = i;
+								dispLastPlayerY = j;
+								System.out.println("LAST: " + dispLastPlayerX + " " + dispLastPlayerY);
+								dispList[i][j].setIcon(
+										new ImageIcon("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/chipRight1.png"));
+							} else if (p.x == x && p.y == y) {
+								int newPosOldToken = x;
+								switch (moveDirection) {
+								case "Left":
+									newPosOldToken = 0;
+								case "Right":
+									newPosOldToken = 1;
+								case "Up":
+									newPosOldToken = 2;
+								case "Down":
+									newPosOldToken = 3;
 								}
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
+								dispList[i][j].setIcon(
+										new ImageIcon("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/" +oldToken.get(newPosOldToken) + ".png"));
+								dispPlayerX = i;
+								dispPlayerY = j;
+								System.out.println("Current ");
+								System.out.println("oldPosTile ");
+								oldToken.clear();
+								oldToken.add(0, board[x][y-1].getName());
+								oldToken.add(1, board[x][y+1].getName());
+								oldToken.add(2, board[x-1][y].getName());
+								oldToken.add(3, board[x+1][y].getName());
+								System.out.println(oldToken);
+								System.out.println(board[x][y].getName());
+
+							}else {
+								if (dispList[i][j] != null) {
+
+									dispList[i][j]
+											.setIcon(new ImageIcon("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/"
+													+ board[x][y].getName() + ".png"));
+
+								}
 							}
+							y++;
 						}
-						/*
-						 * 
-						 * if(p.x == i && p.y == j) {
-						 * System.out.println("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/chipRight"
-						 * + count + ".png"); DispTile t = new DispTile(); dispList[i][j] = t; ImageIcon
-						 * ic = new ImageIcon(
-						 * "src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/chipRight" + count + ".png");
-						 * t.setIcon(ic); } else { DispTile t = new DispTile(); dispList[i][j] = t;
-						 * 
-						 * ImageIcon ic = new ImageIcon(
-						 * "src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/" + board[i][j].getName() +
-						 * ".png"); t.setIcon(ic); //add(t); }
-						 */
-						y++;
+						x++;
 					}
-					x++;
 				}
+				if (count == 1) {
+					System.out.println(count);
+					// updates current sport
+					dispList[dispPlayerX][dispPlayerY]
+							.setIcon(new ImageIcon("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/chipRight3.png"));
+					// updates previous spot
+					dispList[dispLastPlayerX][dispLastPlayerY]
+							.setIcon(new ImageIcon("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/chipRight2.png"));
+				} else if (count == 2) {
+					System.out.println(count);
+					// updates current sport
+					dispList[dispPlayerX][dispPlayerY]
+							.setIcon(new ImageIcon("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/chipRight4.png"));
+					// updates previous spot
+					dispList[dispLastPlayerX][dispLastPlayerY]
+							.setIcon(new ImageIcon("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/freeTile.png"));
+
+				} else if (count == 3) {
+					System.out.println(count);
+					// updates current sport
+					dispList[dispPlayerX][dispPlayerY]
+							.setIcon(new ImageIcon("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/chipTile.png"));
+
+				}
+
 				count++;
 
 			}
 		};
 		new Timer(delay, taskPerformer).start();
+
+//LETS FIX THE ANIMATION
+//		// ANIMATION
+//		int delay = 1; // milliseconds
+//		ActionListener taskPerformer = new ActionListener() {
+//			int count = 0;
+//
+//			public void actionPerformed(ActionEvent evt) {
+//				if (count == 4) {
+//					return;
+//				}
+//				System.out.println(count);
+//				// dispList = new DispTile[xSize][ySize];
+//				int x = p.x-4;
+//				for (int i = 0; i < xSize; i++) {
+//					int y = p.y-4;
+//					for (int j = 0; j < ySize; j++) {
+//						if ((p.x == x && p.y == y) && lastPosition != p) {
+//							System.out.println("test");
+//							try {
+//								final Image[] frames = {
+//										ImageIO.read(new File("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/chipRight"
+//												+ 1 + ".png")),
+//										ImageIO.read(new File("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/chipRight"
+//												+ 2 + ".png")),
+//										ImageIO.read(new File("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/chipRight"
+//												+ 3 + ".png")),
+//										ImageIO.read(new File("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/chipRight"
+//												+ 4 + ".png")) };
+//
+//								dispList[i][j].setIcon(new ImageIcon(frames[count]));
+//								//BoardPanel.add(dispList[i][j]);
+//								
+//							} catch (IOException e) {
+//								// TODO Auto-generated catch block
+//								e.printStackTrace();
+//							}
+//
+//						} else {
+//							try {
+//								Image[] frames = {
+//										ImageIO.read(new File("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/"
+//												+ board[x][y].getName() + ".png")),
+//										ImageIO.read(new File("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/"
+//												+ board[x][y].getName() + ".png")),
+//										ImageIO.read(new File("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/"
+//												+ board[x][y].getName() + ".png")),
+//										ImageIO.read(new File("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/"
+//												+ board[x][y].getName() + ".png")) };
+//								if (dispList[i][j] != null) {
+//									System.out.println("spam");
+//
+//									
+//									dispList[i][j].setIcon(new ImageIcon(frames[count]));
+//									//BoardPanel.add(dispList[i][j]);
+//									
+//								}
+//							} catch (IOException e) {
+//								// TODO Auto-generated catch block
+//								e.printStackTrace();
+//							}
+//						}
+//						/*
+//						 * 
+//						 * if(p.x == i && p.y == j) {
+//						 * System.out.println("src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/chipRight"
+//						 * + count + ".png"); DispTile t = new DispTile(); dispList[i][j] = t; ImageIcon
+//						 * ic = new ImageIcon(
+//						 * "src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/chipRight" + count + ".png");
+//						 * t.setIcon(ic); } else { DispTile t = new DispTile(); dispList[i][j] = t;
+//						 * 
+//						 * ImageIcon ic = new ImageIcon(
+//						 * "src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/" + board[i][j].getName() +
+//						 * ".png"); t.setIcon(ic); //add(t); }
+//						 */
+//						y++;
+//					}
+//					x++;
+//				}
+//				count++;
+//
+//			}
+//		};
+//		new Timer(delay, taskPerformer).start();
 
 		/*
 		 * Old stuff this.maze = maze; board = maze.getBoard(); Point p =
@@ -205,11 +311,26 @@ class View {
 		 * "src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/" + board[i][j].getName() +
 		 * ".png")); } } }
 		 */
-		//add your elements
-		//BoardPanel.revalidate();
-
+		// add your elements
+		// BoardPanel.revalidate();
+		oldBoard = new Board(maze.getBoard());
+		lastPosition = maze.getPlayerPosition();
 		BoardPanel.repaint();
 
+	}
+
+	public String findDirection(Point p) {
+		if (lastPosition.x > p.x) {
+			return "Up";
+		} else if (lastPosition.x < p.x) {
+			return "Down";
+		} else if (lastPosition.y > p.y) {
+			return "Left";
+		} else if (lastPosition.y < p.y) {
+			return "Right";
+		}
+
+		return null;
 	}
 
 	public void updateLastPosition() {
@@ -233,7 +354,6 @@ class View {
 	 * @return - returns a JPanel with the current game state displayed.
 	 */
 	public JPanel getBoardPanel() {
-		System.out.println("Get board Panel " + BoardPanel);
 		return BoardPanel;
 	}
 
@@ -249,35 +369,41 @@ class View {
 			GridLayout layout = new GridLayout(9, 9);
 			setLayout(layout);
 			Point p = maze.getPlayerPosition();
-			
-			int x = p.x-4;
+
+			int x = p.x - 4;
 			for (int i = 0; i < xSize; i++) {
-				int y = p.y-4;
+				int y = p.y - 4;
 				for (int j = 0; j < ySize; j++) {
+					if(x == p.x && y == p.y) {
+						oldToken.add(0, board[x][y-1].getName());
+						oldToken.add(1, board[x][y+1].getName());
+						oldToken.add(2, board[x-1][y].getName());
+						oldToken.add(3, board[x+1][y].getName());
+						System.out.println(oldToken);
+					}
 
 					DispTile t = new DispTile();
 					dispList[i][j] = t;
 					t.setIcon(new ImageIcon(
 							"src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/" + board[x][y].getName() + ".png"));
-					
-					System.out.println("SIZES: " + board.length + " " + board[0].length);
-					System.out.println(x + " " + y);
+
 					add(dispList[i][j]);
-					
+
 					y++;
 				}
 				x++;
 			}
-/*
-			for (int i = p.x - 4; i <= p.x + 4; i++) {
-				for (int j = p.y - 4; j <= p.y + 4; j++) {
-					add(dispList[i][j]);
 
-					
-
-				}
-			} */
+			lastPosition = maze.getPlayerPosition();
 			
+			/*
+			 * for (int i = p.x - 4; i <= p.x + 4; i++) { for (int j = p.y - 4; j <= p.y +
+			 * 4; j++) { add(dispList[i][j]);
+			 * 
+			 * 
+			 * 
+			 * } }
+			 */
 
 //			// GridLayout layout = new GridLayout(xSize - 6, ySize - 6);
 //			GridLayout layout = new GridLayout(9, 9);
