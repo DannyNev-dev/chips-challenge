de6caefa86271a1e8e12883145e518efaab53028
Jordan <jordan_890@hotmail.com>
2020-10-16 14:47:55 +1300

    Added "Focus" to Chip. Changed Blue Door Tile
    Added Focus to Chip. Meaning the "Camera" only displays 9 tiles on the screen now. (4 in each direction either side of chip).
    The focus also follows chip and he moves.
    
    Changed the blue door tile to the correct one.

--- src/nz/ac/vuw/ecs/swen225/gp20/render/Render.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/render/Render.java
@@ -3,6 +3,7 @@ package nz.ac.vuw.ecs.swen225.gp20.render;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Point;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -11,25 +12,30 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Maze.SpecialEvent;
 import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.Tile;
+
 /**
- * Render class
- * This class renders the maze and provides a return method for the GUI.
+ * Render class This class renders the maze and provides a return method for the
+ * GUI.
+ * 
  * @author Jordan Simmons
  **/
 public class Render {
 
 	private View view;
 	private Maze maze;
+
 	/**
 	 * Constructor for the the Render function
+	 * 
 	 * @param maze - this is the game state pulled from maze package
 	 */
 	public Render(Maze maze) {
 		this.maze = maze;
 		view = new View(maze);
 	}
-	
+
 	/**
 	 * This updates the JPanel so it can be returned to Application package
 	 */
@@ -37,9 +43,10 @@ public class Render {
 		maze.getBoard();
 		view.refresh(maze);
 	}
-	
+
 	/**
 	 * Returns the current JPanel to Application package
+	 * 
 	 * @return JPanel of the updated gameboard
 	 */
 	public JPanel getView() {
@@ -49,6 +56,7 @@ public class Render {
 
 /**
  * View Class creates a JPanel to return to Application Package
+ * 
  * @author Jordan Simmons
  *
  */
@@ -63,9 +71,10 @@ class View {
 	private int tileSize = 50;
 	private final static int GAP = 2;
 	private BoardPanel BoardPanel;
-	
+
 	/**
 	 * Constructor for View
+	 * 
 	 * @param maze - the game state from the maze application
 	 */
 	View(Maze maze) {
@@ -76,25 +85,25 @@ class View {
 		dispList = new DispTile[xSize][ySize];
 		createAndShowGUI();
 	}
-	
+
 	/**
-	 * Refreshes the view of the maze. This is called when a change in the maze occurs
+	 * Refreshes the view of the maze. This is called when a change in the maze
+	 * occurs
+	 * 
 	 * @param maze - the game state from the maze application
 	 */
 	void refresh(Maze maze) {
-		this.maze = maze;
-		board = maze.getBoard();
-		for (int i = 0; i < dispList.length; i++) {
-			for (int j = 0; j < dispList[0].length; j++) {
-				if (dispList[i][j] != null) {
-					dispList[i][j].setIcon(new ImageIcon(
-							"src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/" + board[i][j].getName() + ".png"));
-				}
-			}
-		}
-		BoardPanel.repaint();
+		/*
+		 * this.maze = maze; board = maze.getBoard(); Point p =
+		 * maze.getPlayerPosition(); for (int i = p.x-4; i <= p.x+4; i++) {
+		 * 
+		 * for (int j = p.y-4; j <= p.y+4; j++) { if (dispList[i][j] != null) {
+		 * dispList[i][j].setIcon(new ImageIcon(
+		 * "src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/" + board[i][j].getName() +
+		 * ".png")); } } } BoardPanel.repaint();
+		 */
 	}
-	
+
 	/**
 	 * Creates the JPanel to be returned
 	 */
@@ -108,40 +117,53 @@ class View {
 
 	/**
 	 * Gets the main panel so it can be returned to the Application Package
+	 * 
 	 * @return - returns a JPanel with the current game state displayed.
 	 */
 	public JPanel getBoardPanel() {
 		return BoardPanel;
 	}
-	
+
 	/**
-	 * BoardPanel class creates the actual JPanel which is returned to Application package
+	 * BoardPanel class creates the actual JPanel which is returned to Application
+	 * package
+	 * 
 	 * @author Jordan Simmons
 	 *
 	 */
 	class BoardPanel extends JPanel {
 		BoardPanel() {
-			//setBorder(BorderFactory.createLineBorder(Color.BLACK, GAP));
-			GridLayout layout = new GridLayout(xSize - 6, ySize - 6);
+			// GridLayout layout = new GridLayout(xSize - 6, ySize - 6);
+			GridLayout layout = new GridLayout(9, 9);
 			setLayout(layout);
+			Point p = maze.getPlayerPosition();
+			/*
+			 * TODO Add in if statement to get player position correct
+			 *
+			 *
+			 */
 
-			for (int i = 3; i < xSize - 3; i++) {
+			for (int i = p.x - 4; i <= p.x + 4; i++) {
 
-				for (int j = 3; j < ySize - 3; j++) {
+				for (int j = p.y - 4; j <= p.y + 4; j++) {
 					DispTile t = new DispTile();
 					dispList[i][j] = t;
+
 					ImageIcon ic = new ImageIcon(
 							"src/nz/ac/vuw/ecs/swen225/gp20/render/TileFile/" + board[i][j].getName() + ".png");
 					t.setIcon(ic);
 					add(t);
+
 				}
 			}
 
 		}
 	}
-	
+
 	/**
-	 * This creates the tiles to be displayed using the tiles passed from the maze application.
+	 * This creates the tiles to be displayed using the tiles passed from the maze
+	 * application.
+	 * 
 	 * @author Jordan Simmons
 	 *
 	 */
@@ -149,7 +171,6 @@ class View {
 
 		DispTile() {
 			setPreferredSize(new Dimension(tileSize, tileSize));
-			//setBorder(BorderFactory.createLineBorder(Color.BLACK, GAP));
 		}
 	}
 
