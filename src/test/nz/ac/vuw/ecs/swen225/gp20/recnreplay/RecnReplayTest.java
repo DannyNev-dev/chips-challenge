a5c64b486a26a07d1e4eebecd6aa0d4d0884a472
Fay Lu <fay.lu@outlook.com>
2020-10-19 12:18:04 +1300

    add junit to test recnreplay, only skeleton

---
+++ src/test/nz/ac/vuw/ecs/swen225/gp20/recnreplay/RecnReplayTest.java
@@ -0,0 +1,26 @@
+package test.nz.ac.vuw.ecs.swen225.gp20.recnreplay;
+
+
+/**
+ * Test record and replay functions.
+ * @author YanLu
+ *
+ */
+public class RecnReplayTest {
+	
+	@test
+	public void recordTest() {
+		
+	}
+	
+	@test
+	public void deserializationTest() {
+		
+	}
+	
+	@test
+	public void replayTest() {
+		
+	}
+
+}
